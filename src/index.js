83b4bf3bdf1051b5f220f5e7717f253ca8235e1a
Here East <nastiapiven@gmail.com>
2021-03-11 21:41:54 +0100

    feat: update code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 module.exports = function toReadable (number) {
-  let zeroToNineteen = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
+  let one = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
                       'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-  let twentyToNinety = ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let ten = ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
   let num = String(number); 
 
+  if (number === 0) return 'zero';
+
   if (number < 20) {
-      return zeroToNineteen[number];
+      return one[number];
   }
 
   if (number >= 20 && number <= 99) {
-      return twentyToNinety[num[0]] + ' ' + zeroToNineteen[num[1]];
+      return ten[num[0]] + ' ' + one[num[1]];
+  }
+
+  if (number >= 100) {
+      return one[num[0]] + ' hundred' + ten[num[1]] + ' ' + one[num[2]];
   }
 }
 
