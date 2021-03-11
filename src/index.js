da313ce80c2cfdfe21df49ed8fd0da7e91923f87
Here East <nastiapiven@gmail.com>
2021-03-11 22:04:37 +0100

    feat: update code

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 module.exports = function toReadable (number) {
-  let one = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
+  let oneToNineteen = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
                       'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-  let ten = ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let twenties = ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
   let num = String(number); 
 
   if (number === 0) return 'zero';
 
   if (number < 20) {
-      return one[number];
+      return oneToNineteen[number];
   }
 
-  if (number >= 20 && number <= 99) {
-      return ten[num[0]] + ' ' + one[num[1]];
+  if (number < 100) {
+      if (num[1] === '0') {
+        return twenties[num[0]];
+      }
+      return twenties[num[0]] + ' ' + oneToNineteen[num[1]];
   }
 
-  if (number >= 100) {
-      return one[num[0]] + ' hundred' + ten[num[1]] + ' ' + one[num[2]];
+  /* if (number >= 100) {
+      return oneToNineteen[num[0]] + ' hundred' + twenties[num[1]] + ' ' + oneToNineteen[num[2]];
   }
+
+  if (num[1] == 0 && num[2] == 0) {
+      return oneToNineteen[num[0]] + ' hundred';
+  } */
 }
 
