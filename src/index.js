9dd3422796dab88398b55eaa5956f0743ef3d16f
Here East <nastiapiven@gmail.com>
2021-03-11 21:25:53 +0100

    feat: update code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 module.exports = function toReadable (number) {
   let zeroToNineteen = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
-                      'eleven', 'twelve', 'thirteen', 'fourteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+                      'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let twentyToNinety = ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  let num = String(number); 
 
   if (number < 20) {
       return zeroToNineteen[number];
+  }
+
+  if (number >= 20 && number <= 99) {
+      return twentyToNinety[num[0]] + ' ' + zeroToNineteen[num[1]];
   }
 }
 
