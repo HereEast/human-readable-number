09c6da06aa6a5edacf8c5b11ad93d55ed3c4e78f
Here East <nastiapiven@gmail.com>
2021-03-12 10:30:29 +0100

    feat: finish problem

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function toReadable (number) {
   let oneToNineteen = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
                       'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-  let twenties = ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let twenties = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
   let num = String(number); 
 
@@ -18,11 +18,18 @@ module.exports = function toReadable (number) {
       return twenties[num[0]] + ' ' + oneToNineteen[num[1]];
   }
 
-  if (number > 100) {
+if (number >= 100) {
       if (num[1] === '0' && num[2] === '0') {
           return oneToNineteen[num[0]] + ' hundred';
       } else if (num[2] === '0') {
+          if (num[1] === '1') {
+              return oneToNineteen[num[0]] + ' hundred' + ' ' + oneToNineteen[num[1] + num[2]];
+          }
         return oneToNineteen[num[0]] + ' hundred' + ' ' + twenties[num[1]];
+      } else if (num[1] === '0') {
+        return oneToNineteen[num[0]] + ' hundred' + ' ' + oneToNineteen[num[2]];
+      } else if (num[1] === '1') {
+        return oneToNineteen[num[0]] + ' hundred' + ' ' + oneToNineteen[num[1] + num[2]];
       }
       return oneToNineteen[num[0]] + ' hundred' + ' ' + twenties[num[1]] + ' ' + oneToNineteen[num[2]];
   }
